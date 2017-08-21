99ddd91102a1ce06c6e56bc1db8ac0b238a7c7cb
sdvcrx <memory.silentvoyage@gmail.com>
2017-08-21 22:43:24 +0800

    Add simple cache driver implement

---
+++ simpleCacheDriver.js
@@ -0,0 +1,32 @@
+const CacheDriver = require('./cacheDriver')
+const CacheItem = require('./cacheItem')
+
+class SimpleCacheDriver extends CacheDriver {
+  constructor() {
+    super()
+    this._cache = {}
+  }
+
+  get(key) {
+    return new Promise((resolve) => {
+      const item = this._cache[key]
+      if (item && !item.invalid()) {
+        resolve(item)
+        return item
+      }
+      const _item = new CacheItem()
+      resolve(_item)
+    })
+  }
+
+  set(key, data) {
+    return new Promise((resolve) => {
+      const item = new CacheItem(key, data)
+      this._cache[key] = item
+      resolve(item)
+      return item
+    })
+  }
+}
+
+module.exports = new SimpleCacheDriver()
